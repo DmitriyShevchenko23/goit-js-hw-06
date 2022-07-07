bf0f89244135cc31210b5c84fc4261c886b7f6a9
DmitriyShevchenko23 <dmitryshevchenko23@gmail.com>
2022-07-07 18:02:37 +0500

    task 03 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemsList = document.querySelector('#ingredients');
+
+const getItems = items => items.map(item => {
+  const itemEl = document.createElement('li')
+  itemEl.classList.add('item')
+  itemEl.textContent = `${item}`
+  return itemEl;
+})
+
+const ingredientsItems = getItems(ingredients);
+itemsList.append(...ingredientsItems)
+
+console.log(itemsList)
