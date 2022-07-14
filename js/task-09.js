afdfc36a42fa4f762eafbec9cb2ad1473153b4d7
DmitriyShevchenko23 <dmitryshevchenko23@gmail.com>
2022-07-14 19:16:32 +0500

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+
+let color = document.querySelector('.color');
+
+
+buttonEl.addEventListener('click', onButtonClick)
+
+function onButtonClick() {
+  body.style.backgroundColor = getRandomHexColor()
+  color.textContent=`${body.style.backgroundColor}`
+}
