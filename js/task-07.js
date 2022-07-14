1dce09be5efb17bce86244a5986fd28fffe71947
DmitriyShevchenko23 <dmitryshevchenko23@gmail.com>
2022-07-14 18:09:01 +0500

    task-07 done

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const fontSizeEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
-console.log(fontSizeEl, textEl)
+
 
 fontSizeEl.addEventListener('input', (event) => {
     textEl.style.fontSize = `${event.currentTarget.value}px`;
