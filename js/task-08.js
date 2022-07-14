ef4add6381a82e684a3d81af8a7e11b38fc750b7
DmitriyShevchenko23 <dmitryshevchenko23@gmail.com>
2022-07-14 18:49:29 +0500

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit',onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        mail,
+        password
+    };
+
+    if (mail === '' && password === '') {
+        alert('Insert your mail and password')
+       
+    };
+    formEl.reset();
+
+    console.log(formData);
+};
